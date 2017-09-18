6e16e59ec6403978ad9ece4c7078acb037fb7022
lvsam <lucillevsam@gmail.com>
2017-09-18 21:40:54 +0000

    new db comment added

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,8 +1,9 @@
 class Place < ApplicationRecord
+    belongs_to :user
+    has_many :comments
     paginates_per 3
     geocoded_by :address
     after_validation :geocode
-    belongs_to :user
     validates :name , presence: true, length: { minimum: 3 }
     validates :address, :description, presence: true
 end
